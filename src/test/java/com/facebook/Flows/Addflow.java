7fb2eac5123a255c72da6fa9fe0a13c457c304a1
pravin <psarkate94@gmail.com>
2020-03-14 19:49:08 +0530

    added new class and one method in login class

---
+++ src/test/java/com/facebook/Flows/Addflow.java
@@ -0,0 +1,12 @@
+package com.facebook.Flows;
+
+public class Addflow 
+{
+	
+
+
+	public void m1()
+	{
+		System.out.println("add flow");
+	}
+}
--- src/test/java/com/facebook/Flows/Login_Flow.java
+++ src/test/java/com/facebook/Flows/Login_Flow.java
@@ -28,5 +28,16 @@ public class Login_Flow
 		Extent_Reporting.Log_Pass_with_Screenshot("Login", "entered mobileno", driver);
 			
 	}
+	public void loginde(WebDriver driver,String TC_ID) throws Throwable
+	{
+		method.enterText(driver, Login_Loactors.firstname, Excel_Handling.Get_Data(TC_ID, "firstname"), "entered first name");
+		Extent_Reporting.Log_Pass_with_Screenshot("Login", "entered username", driver);
+		method.enterText(driver, Login_Loactors.surname, Excel_Handling.Get_Data(TC_ID, "SurName"), "entered first name");
+		System.out.println("surname form excel"+Excel_Handling.Get_Data(TC_ID, "SurName"));
+		Extent_Reporting.Log_Pass_with_Screenshot("Login", "entered surname", driver);
+		method.enterText(driver, Login_Loactors.mobileno, Excel_Handling.Get_Data(TC_ID, "MobileNo"), "entered first name");
+		Extent_Reporting.Log_Pass_with_Screenshot("Login", "entered mobileno", driver);
+			
+	}
 
 }
